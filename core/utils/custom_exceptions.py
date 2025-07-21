d8ccddacf11ede95a199af9d22221de5eadff6e3
Thiago Arthur <thlimabsgim@gmail.com>
2025-07-21 12:34:53 -0400

    initial commit

---
+++ core/utils/custom_exceptions.py
@@ -0,0 +1,17 @@
+from rest_framework.views import exception_handler
+
+def custom_exception_handler(exc, context):
+    """
+    Custom exception handler to return a more user-friendly error response.
+    """
+    response = exception_handler(exc, context)
+
+    ## Manter um padrão de resposta para erros
+    if response is not None:
+        if response.data.get('messages'):
+            del response.data['messages']
+            
+        if response.data.get('sucess') is None:
+            response.data['success'] = False
+    
+    return response
